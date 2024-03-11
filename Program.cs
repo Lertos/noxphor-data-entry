646d84c82940efdf760b207525b10343d192b203
Lertos <deetasticgaming@gmail.com>
2024-03-10 22:43:45 -0300

    Adding logic to access a new menu

--- Program.cs
+++ Program.cs
@@ -40,15 +40,7 @@ bool HandleInput()
 
     if (!Int32.TryParse(key.ToString(), out digit))
     {
-        if (key == 'q' || key == 'Q')
-        {
-            Environment.Exit(0);
-        }
-        else
-        {
-            errorMessage = "That is not a valid key input";
-            return true;
-        }
+        HandleSharedKey(key);
     }
 
     switch (digit)
@@ -64,7 +56,85 @@ bool HandleInput()
     return true;
 }
 
+void HandleSharedKey(char key)
+{
+    if (key == 'q' || key == 'Q')
+    {
+        Console.Clear();
+        Console.WriteLine("Goodbye!");
+
+        Environment.Exit(0);
+    }
+    else if (key == 'b' || key == 'B')
+    {
+        //TODO: Add logic to go back a menu (save previous menu state)
+    }
+    else if (key == 'm' || key == 'M')
+    {
+        MainMenu();
+    }
+    else
+    {
+        errorMessage = "That is not a valid key input";
+    }
+}
+
 void LocationMenu()
 {
+    while (true)
+    {
+        Console.Clear();
 
+        Console.WriteLine("===============================================");
+        Console.WriteLine(" Choose the action you would like to perform.:");
+        Console.WriteLine("===============================================");
+        Console.WriteLine();
+        Console.WriteLine(" 1 - Add New Location");
+        Console.WriteLine(" 2 - Edit Existing Location");
+        Console.WriteLine(" 3 - Delete a Location");
+        Console.WriteLine(" 4 - Add New Location");
+        Console.WriteLine();
+        Console.WriteLine(" b - Back to the Previous Menu");
+        Console.WriteLine(" m - Back to the Main Menu");
+        Console.WriteLine(" q - Quit the application");
+        Console.WriteLine();
+        Console.WriteLine("===============================================");
+
+        if (!string.IsNullOrEmpty(errorMessage))
+        {
+            Console.Write(" ");
+            Console.WriteLine(errorMessage);
+            Console.WriteLine("===============================================");
+
+            errorMessage = "";
+        }
+
+        if (HandleLocationInput())
+            break;
+    }
+}
+
+bool HandleLocationInput()
+{
+    char key = Console.ReadKey(false).KeyChar;
+    int digit;
+
+    if (!Int32.TryParse(key.ToString(), out digit))
+    {
+        HandleSharedKey(key);
+        return true;
+    }
+
+    switch (digit)
+    {
+        case 1:
+            //TODO:
+            break;
+        //TODO: Add other methods
+        default:
+            errorMessage = "That is not a valid key input";
+            return false;
+    }
+
+    return true;
 }
