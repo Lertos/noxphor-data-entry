0fac1aaaa44f4a611712c6cf26601f2f7f734d17
Lertos <deetasticgaming@gmail.com>
2024-03-11 18:21:38 -0300

    Adding base questions to add a new location

--- Program.cs
+++ Program.cs
@@ -92,7 +92,6 @@ void LocationMenu()
         Console.WriteLine(" 1 - Add New Location");
         Console.WriteLine(" 2 - Edit Existing Location");
         Console.WriteLine(" 3 - Delete a Location");
-        Console.WriteLine(" 4 - Add New Location");
         Console.WriteLine();
         Console.WriteLine(" b - Back to the Previous Menu");
         Console.WriteLine(" m - Back to the Main Menu");
@@ -127,14 +126,90 @@ bool HandleLocationInput()
 
     switch (digit)
     {
+        //Add new location
         case 1:
-            //TODO:
+            AddNewLocation();
             break;
-        //TODO: Add other methods
+        //Edit location
+        case 2:
+            EditLocation();
+            break;
+        //Delete location
+        case 3:
+            DeleteLocation();
+            break;
         default:
             errorMessage = "That is not a valid key input";
             return false;
     }
 
     return true;
+}
+
+void AddNewLocation()
+{
+    bool error = true;
+    
+    while (error)
+    {
+        Console.Clear();
+
+        Console.WriteLine("===============================================");
+        Console.WriteLine(" Enter in the information as you are asked:");
+        Console.WriteLine(" b - Cancel and go back to the Previous Menu");
+        Console.WriteLine("===============================================");
+
+        if (!string.IsNullOrEmpty(errorMessage))
+        {
+            Console.Write(" ERROR: ");
+            Console.WriteLine(errorMessage);
+            Console.WriteLine("===============================================");
+
+            errorMessage = "";
+        }
+
+        //ID
+        Console.WriteLine("\n==Enter in the location ID:");
+        string id = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(id))
+        {
+            errorMessage = "Location ID cannot be empty";
+            continue;
+        }
+
+        //Display name
+        Console.WriteLine("\n==Enter in the location display name:");
+        string displayName = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(displayName))
+        {
+            errorMessage = "Location display name cannot be empty";
+            continue;
+        }
+
+        //Description
+        Console.WriteLine("\n==Enter in the location description:");
+        string description = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(description))
+        {
+            errorMessage = "Location description cannot be empty";
+            continue;
+        }
+
+        //TODO: Check if the location already exists. Also check what happens when you save and the ID exists; should replace.
+
+        error = false;
+    }
+}
+
+void EditLocation()
+{
+
+}
+
+void DeleteLocation()
+{
+
 }
